151cdec9692af8b75d11c54ce8fbf5ab71b8780f
Ildus <qwzxaser2015@gmail.com>
2025-06-10 12:42:56 +0300

    Функция в drawRectangle() была заменена на функцию drawBroderRectangle() и было решено в Entity убрать метод getBorders()

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,6 @@
 #include "GL/freeglut.h"
 #include "Game.hpp"
 
-
-// void renderScene();
-
-// void timerFunc(int value);
-
-// void keyfunc(unsigned char K, int x, int y);
-
-// void keyboardUp(unsigned char K, int x, int y);
-
-// void updateScene();
-
 int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
@@ -27,46 +16,3 @@ int main(int argc, char **argv)
     glutMainLoop();
     return 0;
 }
-
-
-// файлы написал, а на гите они пустые пупупу
-
-
-// void updateScene()
-// {
-//     if (PRESSED_KEY_A)
-//         map.movePlatformLeft();
-//     if (PRESSED_KEY_D)
-//         map.movePlatformRight();
-//     map.update();
-//     glutPostRedisplay();
-// }
-
-
-// void keyboardUp(unsigned char K, int x, int y)
-// {
-//     if (K == 'a')
-//         PRESSED_KEY_A = false;
-//     if (K == 'd')
-//         PRESSED_KEY_D = false;
-//     map.resetVelocity();
-// }
-
-// void keyfunc(unsigned char K, int x, int y)
-// {
-//     if (K == 'd')
-//         PRESSED_KEY_D = true;
-//     if (K == 'a')
-//         PRESSED_KEY_A = true;
-// }
-
-// void renderScene()
-// {
-//     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-//     glPushMatrix();
-//     map.draw();
-//     glColor3f(1, 1, 1);
-//     obj.draw();
-//     glPopMatrix();
-//     glutSwapBuffers();
-// }
