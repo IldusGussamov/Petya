67b2a4c0468b5f551f357e71ce8f317f9da6ffc7
motvm <matvey.e38@gmail.com>
2025-06-19 00:57:34 +0300

    Белый экран и вылетает, проблема с map и mapGenerator

--- src/Map.cpp
+++ src/Map.cpp
@@ -1,4 +1,5 @@
 #include "Map.hpp"
+#include "MapGenerator.hpp"
 #include "utils.hpp"
 #include "PowerfulBrick.hpp"
 #include "SimpleBrick.hpp"
@@ -14,77 +15,18 @@ Map::Map(Point position, Size size) : platform(PLATFORM_POSITION, {0, 0})
 {
     this->position = position;
     this->size = size;
+    MapGenerator generator(position, true);
     this->generateBricks();
+    this->bricks = generator.getBricks();
     this->directionThrowBall = 0;
 }
 
 // Генерация кирпичей на карте с использованием случайных значений
 void Map::generateBricks()
 {
-    Point actualPosition = {position.x + BRICK_WIDTH / 2, position.y - BRICk_HEIGHT / 2};
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    for (int i = 0; i < ROWS; i++)
-    {
-        for (int j = 0; j < COLS; j++)
-        {
-            std::uniform_int_distribution<int> dist(1, 120);
-            int randomValue = dist(gen);
-
-            // Добавление различных типов кирпичей по вероятности
-            if (randomValue < 3)
-            {
-                bricks.push_back(new MagicBrick(actualPosition, BRICK_WIDTH, BRICk_HEIGHT, 3));
-            }
-            if (randomValue >= 113)
-            {
-                bricks.push_back(new MagicBrick(actualPosition, BRICK_WIDTH, BRICk_HEIGHT, 2));
-            }
-            if (randomValue >= 3 and randomValue < 4)
-            {
-                bricks.push_back(new MagicBrick(actualPosition, BRICK_WIDTH, BRICk_HEIGHT, 1));
-            }
-            if (randomValue >= 70 and randomValue < 75)
-            {
-                bricks.push_back(new SimpleBrick(actualPosition, BRICK_WIDTH, BRICk_HEIGHT));
-            }
-            if (randomValue >= 75 and randomValue < 85)
-            {
-                bricks.push_back(new SeriousBrick(actualPosition, BRICK_WIDTH, BRICk_HEIGHT));
-            }
-            if (randomValue >= 85 and randomValue < 90)
-            {
-                bricks.push_back(new PowerfulBrick(actualPosition, BRICK_WIDTH, BRICk_HEIGHT));
-            }
-            // Добавление боевых кирпичей в центре поля
-            if ((ROWS - ROWS / 5 > i and i > ROWS / 5) and (COLS - COLS / 5 > j and j > COLS / 5))
-            {
-                if (randomValue >= 90 and randomValue < 99)
-                {
-                    bricks.push_back(new CombatBrick(actualPosition, BRICK_WIDTH, BRICk_HEIGHT, 1));
-                }
-                if (randomValue >= 99 && randomValue < 110)
-                {
-                    bricks.push_back(new CombatBrick(actualPosition, BRICK_WIDTH, BRICk_HEIGHT, 2));
-                }
-                if (randomValue >= 110 && randomValue <= 113)
-                {
-                    bricks.push_back(new CombatBrick(actualPosition, BRICK_WIDTH, BRICk_HEIGHT, 2));
-                }
-            }
-            // Случайно делаем некоторые кирпичи движущимися
-            if (!bricks.empty() && dist(gen) > 90)
-            {
-                Coordinate speed = BALL_SPEED * 0.1;
-                if (dynamic_cast<CombatBrick*>(bricks.back()))
-                    speed *= 0.4;
-                bricks.back()->setMovePattern(speed, PLATFORM_WIDTH, dist(gen) % 4);
-            }
-            actualPosition.x += BRICK_WIDTH;
-        }
-        actualPosition.x = position.x + BRICK_WIDTH / 2;
-        actualPosition.y -= BRICk_HEIGHT;
-    }
+    MapGenerator generator(position, true);
+    generator.generateBricks();
+    this->bricks = generator.getBricks();
 }
 
 // Отрисовка карты, платформы, кирпичей, шаров, бонусов и ракет
